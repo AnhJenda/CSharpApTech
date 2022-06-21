56a40cfd8300cacc3d5af4bb1690f704dd5a3c9c
AnhJenda <100700718+TinyTots-ePj@users.noreply.github.com>
2022-06-21 09:17:01 +0700

    update

---
+++ repos/Session01/AnonTypes/AnonTypes.cs
@@ -0,0 +1,9 @@
+﻿class AnonTypes
+{
+    static void Main(String[] args)
+    {
+        var p1 = new { Name = "A", price = 3 };
+        Console.WriteLine("Name = {0}\nPrice = {1}", p1.Name.ToLower(), p1.price);
+        Console.ReadLine();
+    }
+}
---
+++ repos/Session01/ImplicitilyTypedLocal/ImplicitilyTypedLocal.cs
@@ -0,0 +1,14 @@
+﻿class ImplicitilyTypedLocal 
+{
+    static void Main(String[] args)
+    {
+        var i = 5;
+        var s = "hello";
+        var d = 1.0;
+
+        Console.WriteLine("i*i : " + i*i);
+        Console.WriteLine("s in uppercase: " + s.ToUpper());
+        Console.WriteLine("type of d: " + d.GetType());
+        Console.ReadLine();
+    }
+}

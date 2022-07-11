ccf86e3a2f6d8ddbf2340947ec1d2d98e8e078e7
AnhJenda <100700718+TinyTots-ePj@users.noreply.github.com>
2022-07-11 20:50:03 +0700

    update

---
+++ repos/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,70 @@
+﻿class People
+{
+    protected string Name
+    {
+        get; set;
+    }
+    protected int Age
+    {
+        get; set;
+    }
+    protected string Gender
+    {
+        get; set;
+    }
+
+    public void displayInfo()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+        Console.WriteLine("Gender: " + Gender);
+    }
+}
+
+class Student : People
+{
+    public float Score
+    {
+        get; set;
+    }
+
+    new void DisplayInfo()
+    {
+        base.displayInfo();
+        Console.WriteLine("Student test score: " + Score);
+    }
+/*    public static void Main(string[] args)
+    {
+        Student objStudent = new Student();
+        objStudent.Name = "Dinh Quang Anh";
+        objStudent.Age = 10;
+        objStudent.Gender = "Male";
+        objStudent.Score = 5.75F;
+
+        objStudent.DisplayInfo();
+    }*/
+}
+
+class Staff : People
+{
+    public double Salary
+    {
+        get; set;
+    }
+    new void DisplayInfo()
+    {
+        base.displayInfo();
+        Console.WriteLine("Salary : " + Salary);
+    }
+    public static void Main(string[] args)
+    {
+
+        Staff objStaff = new Staff();
+        objStaff.Name = "Dinh Quang Em";
+        objStaff.Age = 30;
+        objStaff.Gender = "Female";
+        objStaff.Salary = 20000000;
+
+        objStaff.DisplayInfo();
+    }
+}
---
+++ repos/Session11/CodeSnippet9/CodeSnippet9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+class GenealList<T>
+{
+    protected T ItemOne;
+    public GenealList(T valOne)
+    {
+        ItemOne = valOne;
+    }
+    public virtual T GetValue()
+    {
+        return ItemOne;
+    }
+}
+class Student<T> : GenealList<T>
+{
+    public T Value;
+    public Student(T valOne, T valTwo) : base(valOne)
+    {
+        Value = valTwo;
+    }
+    public override T GetValue()
+    {
+        Console.Write(base.GetValue() + "\t\t");
+        return Value;
+    }
+}
+class StudentList
+{
+    public static void Main()
+    {
+        Student<string> objStudent = new Student<string>("Patrick", "Male");
+        Console.WriteLine("Name\t\tSex");
+        Console.WriteLine(objStudent.GetValue());
+    }
+}
